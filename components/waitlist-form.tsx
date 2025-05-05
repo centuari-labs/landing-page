b029dec7e5c7104068f8d25f67e2155a6a39af30
ijul <hi@afrijal.dev>
2025-05-05 21:29:28 +0700

    fix button

--- components/waitlist-form.tsx
+++ components/waitlist-form.tsx
@@ -14,6 +14,11 @@ export default function WaitlistForm() {
 
   const { executeRecaptcha } = useReCaptcha();
 
+  const isValidEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   const handleSubmit = useCallback(
     async (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
@@ -54,24 +59,51 @@ export default function WaitlistForm() {
       onSubmit={handleSubmit}
       className="md:w-1/3 w-full flex flex-col items-center justify-center"
     >
-      <div className="flex items-center relative md:w-full w-2/3 justify-center gap-3 mt-8 md:mt-8 md:flex-row">
-        <Input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder="jhon@centuari.com"
-          className={cn(
-            "rounded-full md:h-14 h-12 border-[#1084AB] text-xs md:text-sm px-3 ring-[#0C63BA] placeholder:text-xs md:placeholder:text-sm",
-            isError ? "border-red-500 focus:ring-red-600" : ""
-          )}
-        />
+      <div className="flex items-center flex-col justify-center w-full">
+        <div className="flex items-center relative w-full justify-center gap-3 mt-8 md:mt-8 md:flex-row">
+          <Input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder="jhon@centuari.com"
+            className={cn(
+              "rounded-full md:h-14 h-12 border-[#1084AB] text-xs md:text-sm px-3 ring-[#0C63BA] placeholder:text-xs md:placeholder:text-sm",
+              isError ? "border-red-500 focus:ring-red-600" : ""
+            )}
+          />
+          <Button
+            type="submit"
+            variant={isError ? "destructive" : "colorful"}
+            className="cursor-pointer rounded-full absolute right-2 text-xs md:text-sm my-2 hidden md:block"
+            disabled={loading || !email || !isValidEmail(email)}
+          >
+            {loading ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-loader-circle-icon lucide-loader-circle animate-spin"
+              >
+                <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+              </svg>
+            ) : (
+              "Subscribe"
+            )}
+          </Button>
+        </div>
         <Button
           type="submit"
           variant={isError ? "destructive" : "colorful"}
-          className="cursor-pointer rounded-full absolute right-2 text-xs md:text-sm my-2"
-          disabled={loading || !email}
+          className="cursor-pointer rounded-full text-xs md:text-sm my-4 w-full md:hidden"
+          disabled={loading || !email || !isValidEmail(email)}
         >
           {loading ? (
             <svg
